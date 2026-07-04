682de6e8eb3e55381b57a5ff8ff8c9b3c31c7e49
Bilal <bilalfathelbab@gmail.com>
2026-07-04 11:17:17 -0500

    fixed zoom bug

--- js/main.js
+++ js/main.js
@@ -26,3 +26,15 @@ if (document.readyState === 'loading') {
 } else {
   initialize();
 }
+
+let lastTouchEnd = 0;
+
+document.addEventListener("touchend", function (event) {
+    const now = Date.now();
+
+    if (now - lastTouchEnd <= 300) {
+        event.preventDefault();
+    }
+
+    lastTouchEnd = now;
+}, { passive: false });
